a6fa0d961eeb8f997e284974904920ce8ca13997
Leo Khachatorians <leokhachatorians@gmail.com>
2015-12-20 18:27:34 -0800

    added a basic server

---
+++ basic_server.py
@@ -0,0 +1,30 @@
+import bluetooth as bluetooth
+import multiprocessing
+
+def receive_messages(sock):
+	try:
+		while True:
+			print('Received Messages: {0}'.format(sock.recv(1024)))
+		except bt.btcommon.BluetoothError as e:
+			raise
+
+port = 1
+backlog = 1
+
+serv = bt.BluetoothSocket(bt.RFCOMM)
+serv.bind(("", port))
+serv.listen(backlog)
+
+client_s, client_i = serv.accept()
+print('Succesfully connected with: ', client_i)
+
+try:
+	check_messages = multiprocessing.Process(target=receive_messages,
+		args=(client_s,))
+	check_messages.start()
+
+	while True:
+		message = input('Message: ')
+		client_s.send(message)
+except Exception as e:
+	print('Error: {0}'.format(e))
