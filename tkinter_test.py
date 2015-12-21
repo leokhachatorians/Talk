fc2b601ad5d192f13e761cb892b393f76c2f839c
Leo Khachatorians <leokhachatorians@gmail.com>
2015-12-20 18:10:43 -0800

    various GUI modifications

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,33 +1,67 @@
-from tkinter import *
+import tkinter as tk
 import bluetooth as bt
 import multiprocessing
 import threading
-from queue import Empty, Full
+#from queue import Empty, Full
+import queue
+import time
 
+'http://stackoverflow.com/questions/13228763/using-multiprocessing-module-for-updating-tkinter-gui'
 SERVER = '5C:F3:70:75:AD:2E'
 PORT = 1
 
-class BlueToothClient():
-	def __init__(self, master, q):
-		self.root = master
+class BlueToothClient(tk.Frame):
+	def __init__(self, root, q):
+		self.root = root
+		self.q = q
+
+		# Menu Bar
+		self.menubar = tk.Menu(root)
+
+		# Add File Tab
+		self.file_menu = tk.Menu(self.menubar, tearoff=0)
+		self.file_menu.add_command(label='Exit')
+		self.menubar.add_cascade(label='File',menu=self.file_menu)
+
+		# Add BlueTooth Tab
+		self.bt_menu = tk.Menu(self.menubar, tearoff=0)
+		self.bt_menu.add_command(label='Scan')
+		self.bt_menu.add_separator()
+		self.bt_menu.add_command(label='Connect To',
+			command=self.create_connect_to_window)
+		self.bt_menu.add_command(label='Host Server',
+			command=self.create_host_server_window)
+		self.menubar.add_cascade(label='BlueTooth',menu=self.bt_menu)
+
+
+		self.root.config(menu=self.menubar)
+
+		# Key Binds
 		self.root.bind('<Return>', self.send_message)
-		frame = Frame(master)
+
+		frame = tk.Frame(root)
 		frame.pack()
 
-		self.sock = self.connect_to(SERVER, PORT)
+		self.sock = None
 
-		self.chat_display = Text(master, width=50)
+		# Chat Receive Display
+		self.chat_display = tk.Text(root, width=50)
 		self.chat_display.configure(state='disabled',font='helvetica 14')
 		self.chat_display.pack(ipady=3)
 
-		self.chat_send = Entry(master, width=50)
+		# Chat Send Display
+		self.chat_send = tk.Entry(root, width=50)
 		self.chat_send.pack(ipady=3)
 		self.chat_send.focus_set()
 
-		self.button = Button(master, text="Enter", width=50,command=self.send_message)
+		# Send Button
+		self.button = tk.Button(root, text="Enter", width=50,command=self.send_message)
 		self.button.pack()
 
-		self.root.after(100,self.check_queue,q)
+		self.root.after(100,self.check_queue)
+
+	def delete_child_window(self, window):
+		window.destroy()
 
 	def display_sent_text(self):
 		self.enable_chat_display_state()
@@ -49,10 +83,11 @@ class BlueToothClient():
 			return False
 		return True
 
-	def connect_to(self, address, port,*,connection=bt.RFCOMM):
+	def connect_to(self, address, port, child_window, *, connection=bt.RFCOMM):
+		child_window.destroy()
 		sock = bt.BluetoothSocket(connection)
 		sock.connect((address,port))
-		return sock
+		self.sock = sock
 
 	def send_message(self, event=None):
 		if self.check_if_not_empty_message():
@@ -62,27 +97,115 @@ class BlueToothClient():
 			self.sock.send(self.chat_send.get())
 			self.clear_chat_send_text()
 
-	def check_queue(self, c_queue):
+	def check_queue(self):
 		try:
-			data = c_queue.get(0).decode('utf-8')
-			#data = data.decode('utf-8')
+			data = self.q.get(0).decode('utf-8')
 			self.enable_chat_display_state()
 			self.chat_display.insert('end',('\nThem: ' + data))
 			self.disable_chat_display_state()
-		except Empty:
+		except queue.Empty:
 			pass
 		finally:
-			self.root.after(100, self.check_queue, c_queue)
+			self.root.after(100, self.check_queue)
+
+	def host_server(self, port, backlog, connection=bt.RFCOMM):
+		server = bt.BluetoothSocket(connection)
+		server.bind(("", port))
+		server.listen(backlog)
+
+		self.enable_chat_display_state()
+		self.chat_display.insert('end', ('Waiting for connection on port: {0}...'.format(port)))
+		self.disable_chat_display_state()
+
+		client_sock, client_info = server.accept()
+
+		self.enable_chat_display_state()
+		self.chat_display.insert('end', ('Connected with: {0}'.format(client_info)))
+		self.disable_chat_display_state()
+
+		return client_sock
+
+	def create_host_server_window(self):
+		scan_window = tk.Toplevel()
+		scan_window.title('Create Host Server')
+		scan_window.lift(aboveThis=self.root)
+
+		port = tk.Entry(scan_window, width=20)
+		port.pack(ipady=3)
+		port.insert(0,'Port')
+		port.focus_set()
+
+		backlog = tk.Entry(scan_window, width=20)
+		backlog.pack(ipady=3)
+		backlog.insert(0,'Backlog')
+
+		button = tk.Button(scan_window, text='Create', width=20,
+			command= lambda: self.host_server(
+				port=int(port.get()),
+				backlog=int(backlog.get())))
+		button.pack()
+
+	def create_connect_to_window(self):
+		connect_to_window = tk.Toplevel()
+		connect_to_window.title('Connect')
+		connect_to_window.lift(aboveThis=self.root)
+
+		address = tk.Entry(connect_to_window, width=20)
+		address.pack(ipady=3)
+		address.insert(0,'Address')
+		address.focus_set()
+
+		port = tk.Entry(connect_to_window, width=20)
+		port.pack(ipady=3)
+		port.insert(0,'Port')
+
+		button = tk.Button(connect_to_window, text='Connect', width=20,
+			command= lambda: self.connect_to(
+				address=address.get(),
+				port=int(port.get()),
+				child_window=connect_to_window))
+		button.pack()
+
+
+class ThreadedClient():
+	def __init__(self, master):
+		self.master = master
+		self.queue = queue.Queue()
+
+		self.gui = BlueToothClient(master, self.queue)
+
+		self.await_messages = threading.Thread(target=self.await_messages_thread)
+		self.await_messages.start()
+		self.periodic_call()
+
+	def periodic_call(self):
+		self.gui.check_queue()
+		self.master.after(100, self.periodic_call)
+
+	def start_await_messages_thread(self):
+		self.await_messages.start()
+
+	def await_messages_thread(self):
+		while True:
+			try:
+				self.queue.put(self.gui.sock.recv(1024))
+			except AttributeError:
+				time.sleep(2)
+				pass
 
 def receive_message(q, sock):
 	while True:
 		q.put(sock.recv(1025))
 
 if __name__ == '__main__':
-	root = Tk()
-	q = multiprocessing.Queue()
-	q.cancel_join_thread()
-	app = BlueToothClient(root, q)
-	t1 = multiprocessing.Process(target=receive_message,args=(q,app.sock))
-	t1.start()
+	root = tk.Tk()
+	client = ThreadedClient(root)
 	root.mainloop()
+	# root = tk.Tk()
+	# q = multiprocessing.Queue()
+	# q.cancel_join_thread()
+	# app = BlueToothClient(root, q)
+	# print(app.sock)
+	# t1 = multiprocessing.Process(target=receive_message,args=(q,app.sock))
+	# t1.start()
+	# root.mainloop()
