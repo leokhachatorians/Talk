3d20bf24d96e58935aa9ca122767a8e33e98a824
leokhachatorians <leokhachatorians@gmail.com>
2016-03-12 22:25:13 -0800

    Add window Title

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from classes.thread_client import ThreadedClient
 
 if __name__ == '__main__':
     master = tk.Tk()
+    master.wm_title("Talk")
     client = ThreadedClient(master)
     master.mainloop()
